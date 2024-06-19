4b5e002065a3055d84270f038ab8f11ef9a8a3c6
Emmanuel Kant Duarte <emmanuelkant.duarte@gmail.com>
2024-06-19 19:33:17 +0100

    refactor the code

--- tests/ps-plus-games-friend.spec.js
+++ tests/ps-plus-games-friend.spec.js
@@ -1,19 +1,42 @@
 require("dotenv").config();
+const fs = require("fs");
 const { test } = require("@playwright/test");
-const axios = require("axios");
-const { PSPlusFriend } = require("./PSPlusFriend");
 
 test("Extract the data", async ({ page }, testInfo) => {
-  const rawData = await axios.get(process.env.REMOTE_DATA_URL);
-  const oldData = rawData.data;
+  await page.goto(process.env.GAMES_URL);
 
-  const psPlusFriend = new PSPlusFriend(page);
+  const contentBlock = page.locator(
+    ".cmp-experiencefragment--your-latest-monthly-games .content-grid"
+  );
 
-  const nextMonth = psPlusFriend.getNextMonth(oldData);
+  const result = [];
 
-  const lastPSPlusPost = await psPlusFriend.getLastPSPlusPost(nextMonth);
+  for (const game of await contentBlock.locator(">*").all()) {
+    const sourceElement = game.locator("source").last();
+    const imgSrc = await sourceElement.getAttribute("srcset");
 
-  const data = await psPlusFriend.getPostData(lastPSPlusPost);
+    const title = await game.locator("h3").innerText();
+    const description = await game.locator("p").innerText();
 
-  await psPlusFriend.saveJSON(data, oldData, testInfo);
+    result.push({
+      title,
+      imgSrc,
+      description,
+    });
+  }
+
+  const date = new Date();
+
+  const file = testInfo.outputPath(
+    `ps-plus-monthly-games-${date.getFullYear()}${
+      date.getMonth() + 1
+    }${date.getDate()}.json`
+  );
+
+  const fullDate = {
+    annoucementDate: date.getTime(),
+    data: result,
+  };
+
+  await fs.promises.writeFile(file, JSON.stringify(fullDate), "utf8");
 });
